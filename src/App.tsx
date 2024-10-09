6dd84e8e2e377e8fe83c9bbedbbdaf1208054c94
k-h08 <popun135@gmail.com>
2024-10-10 00:26:08 +0900

    プロジェクトの作成

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+const App = () => {
+  return (
+    <div>
+      <h1>TriTask</h1>
+      <h2>午前</h2>
+      <ul>
+        <li>午前１</li>
+        <li>午前２</li>
+        <li>午前３</li>
+      </ul>
+      <h2>午後</h2>
+      <h2>毎日</h2>
+      <h2>スキマ</h2>
+      <h2>遊び</h2>
+      <h2>その他</h2>
+    </div>
+  )
+}
+
+export default App
