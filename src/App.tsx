f638989a955741f786774e617230c7a536b2da0f
k-h08 <popun135@gmail.com>
2024-11-01 01:33:32 +0900

    ルーティング、コンポーネントの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-const App = () => {
+import React from "react"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Report from "./pages/Report"
+import NoMatch from "./pages/NoMatch"
+import AppLayout from "./components/layout/AppLayout"
+
+const App: React.FC = () => {
   return (
-    <div>
-      <h1>TriTask</h1>
-      <h2>午前</h2>
-      <ul>
-        <li>午前１</li>
-        <li>午前２</li>
-        <li>午前３</li>
-      </ul>
-      <h2>午後</h2>
-      <h2>毎日</h2>
-      <h2>スキマ</h2>
-      <h2>遊び</h2>
-      <h2>その他</h2>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/report" element={<Report />} />
+          <Route path="/*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
